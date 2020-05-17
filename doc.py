12329f7cca2e168309684c1bee276084c67896bb
bjsh1290576@gn.iwasaki.ac.jp <n1290576@jn2.iwasaki.ac.jp>
2020-05-17 16:31:08 +0900

    initial

---
+++ doc.py
@@ -0,0 +1,38 @@
+
+class MyClass(objsct):
+    def __init__(self):
+        super(MyClass, self).__init__()
+        self.numeric_var = 1
+        
+class MyClass(EventDispatcher):
+    numeric_var = NumericProperty(1)
+    
+"""
+<loginScreen>:
+    f_username: username
+    f_password: password
+    GridLayout:
+        rows: 2
+        cols: 2
+        padding: 10
+        spacing: 10
+        Label:
+            text: 'User Name:'
+        TextInput:
+            id: username
+        label:
+            text: 'Password:'
+        TextInput:
+            id: password
+            password: True
+            
+            
+def on_touch_down(self, touch):
+    if super(OurClassName, self).on_touch_down(touch):
+        return True
+    if not self.collide_point(touch.x, touch.y):
+        return False
+    print('you touched me!')
+    return True
+"""
+
---
+++ main.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+# 画面上の見た目や昨日を構成するクラス
+class MainScreen(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        btn = Button(text="hello")
+        self.add_widget(btn)
+        
+
+# アプリを構成するクラス
+class MainApp(App):
+    def on_start(self):
+        print("App Start!")
+    
+    def build(self):
+        MS = MainScreen()
+        return MS
+
+    def on_stop(self):
+        print("App End!")
+
+if __name__=="__main__":
+    MainApp().run()

6ba236f9f71ef354d1b3c54bc13f64bebef6b773
bjsh1290576@gn.iwasaki.ac.jp <bjsh1290576@gn.iwasaki.ac.jp>
2020-05-17 19:56:11 +0900

    始点と終点の軌跡

--- on_touch_down.py
+++ on_touch_down.py
@@ -2,8 +2,12 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 
 class ClickInput(Widget):
-    def on_touch_move(self, touch):
+    def on_touch_down(self, touch):
         print(touch)
+    def pn_touch_move(self, touch):
+        print(touch)
+    def on_touch_up(self, touch):
+        print("Release!", touch)
         
 class IntroKivy(App):
     def build(self):

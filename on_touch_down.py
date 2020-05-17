28e9664fe925cfc860fc0ba94f6705d5c80efd27
bjsh1290576@gn.iwasaki.ac.jp <bjsh1290576@gn.iwasaki.ac.jp>
2020-05-17 19:54:04 +0900

    ドラッグしている軌跡をトラック

--- on_touch_down.py
+++ on_touch_down.py
@@ -2,8 +2,8 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 
 class ClickInput(Widget):
-    def on_touch_up(self, touch):
-        print("Released!", touch)
+    def on_touch_move(self, touch):
+        print(touch)
         
 class IntroKivy(App):
     def build(self):
